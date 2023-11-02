ad0f09f806abf02e23f1e4b82d1a6e0d669d5e02
eduzkipp <eduzkipp@gmail.com>
2023-11-02 12:14:48 +0300

    logic

---
+++ chat/routing.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from chat.consumers import ChatConsumer
+
+
+
+websocket_urlpatterns=[
+path("",ChatConsumer.as_asgi()),
+
+]
--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import path, include
+from django.urls import path, include
 from chat import views as chat_views
 from django.contrib.auth.views import LoginView,LogoutView
 
